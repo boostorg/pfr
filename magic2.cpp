b8090f7307690e56b3ea9878f2d4cbf67ca1cb0f
Antony Polukhin <antoshkka@gmail.com>
2016-03-22 22:30:00 +0300

    Header fixed

--- magic2.cpp
+++ magic2.cpp
@@ -2,9 +2,6 @@
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-//
-//  See www.boost.org/libs/asio for documentation.
-//
 
 #include <type_traits>
 #include <utility>
