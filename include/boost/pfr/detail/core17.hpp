1bdd543304e0c3203c9b4af6e3f29195f6329df3
Antony Polukhin <antoshkka@gmail.com>
2017-08-19 12:15:49 +0300

    Add a check on broken structured bindings and some notes on workarounds (#9)

--- include/boost/pfr/detail/core17.hpp
+++ include/boost/pfr/detail/core17.hpp
@@ -9,10 +9,38 @@
 
 // TODO: rename as_tuple to tie_as_tuple
 #include <boost/pfr/detail/core17_generated.hpp>
-
 #include <boost/pfr/detail/for_each_field_impl.hpp>
 
 namespace boost { namespace pfr { namespace detail {
+
+struct do_not_define_std_tuple_size_for_me {
+    bool test1 = true;
+};
+
+template <class T>
+constexpr bool do_structured_bindings_work() noexcept { // ******************************************* IN CASE OF ERROR READ THE FOLLOWING LINES IN boost/pfr/detail/core17.hpp FILE:
+    const auto& [a] = T{}; // ******************************************* IN CASE OF ERROR READ THE FOLLOWING LINES IN boost/pfr/detail/core17.hpp FILE:
+
+    /****************************************************************************
+    *
+    * It looks like your compiler or Standard Library can not handle C++17
+    * structured bindings.
+    *
+    * Workaround: Define BOOST_PFR_USE_CPP17 to 0
+    * It will disable the C++17 features for Boost.PFR library.
+    *
+    * Sorry for the inconvenience caused.
+    *
+    ****************************************************************************/
+
+    return a;
+}
+
+static_assert(
+    do_structured_bindings_work<do_not_define_std_tuple_size_for_me>(),
+    "Your compiler can not handle C++17 structured bindings. Read the above comments for workarounds."
+);
+
 
 template <class T, class F, std::size_t... I>
 void for_each_field_dispatcher(T&& t, F&& f, std::index_sequence<I...>) {
