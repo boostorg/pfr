5ffd7f6317565f1e29544f2d4144133b17332b35
Denis Mikhailov <denismikhaylov38@gmail.com>
2021-05-02 17:04:46 +0400

    Update config.hpp

--- include/boost/pfr/detail/config.hpp
+++ include/boost/pfr/detail/config.hpp
@@ -51,7 +51,7 @@
 #   endif
 #endif
 
-#if (BOOST_PFR_USE_CPP17==0 && !BOOST_PFR_USE_LOOPHOLE)
+#if (!BOOST_PFR_USE_CPP17 && !BOOST_PFR_USE_LOOPHOLE)
 #   if (defined(_MSC_VER) && _MSC_VER < 1916) ///< in Visual Studio 2017 v15.9 PFR library with classic engine normally works
 #      error Boost.PFR requires /std:c++latest or /std:c++17 flags on your compiler.
 #   endif
