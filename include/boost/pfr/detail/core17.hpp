9159e4a0b3dda5dd0485cffa0d25b9b4a78153ea
Denis Mikhaylov <denismikhaylov38@gmail.com>
2022-09-06 14:39:30 +0600

    Revert this branch's changes

--- include/boost/pfr/detail/core17.hpp
+++ include/boost/pfr/detail/core17.hpp
@@ -14,33 +14,43 @@
 
 namespace boost { namespace pfr { namespace detail {
 
-struct GuaranteedSimpleAggregate {
+#ifndef _MSC_VER // MSVC fails to compile the following code, but compiles the structured bindings in core17_generated.hpp
+struct do_not_define_std_tuple_size_for_me {
     bool test1 = true;
 };
 
 template <class T>
-constexpr auto tie_as_tuple(T& val) noexcept { // ******************************************* IN CASE OF ERROR READ THE FOLLOWING LINES IN boost/pfr/detail/core17.hpp FILE:
+constexpr bool do_structured_bindings_work() noexcept { // ******************************************* IN CASE OF ERROR READ THE FOLLOWING LINES IN boost/pfr/detail/core17.hpp FILE:
+    T val{};
+    const auto& [a] = val; // ******************************************* IN CASE OF ERROR READ THE FOLLOWING LINES IN boost/pfr/detail/core17.hpp FILE:
+
+    /****************************************************************************
+    *
+    * It looks like your compiler or Standard Library can not handle C++17
+    * structured bindings.
+    *
+    * Workaround: Define BOOST_PFR_USE_CPP17 to 0
+    * It will disable the C++17 features for Boost.PFR library.
+    *
+    * Sorry for the inconvenience caused.
+    *
+    ****************************************************************************/
+
+    return a;
+}
+
+static_assert(
+    do_structured_bindings_work<do_not_define_std_tuple_size_for_me>(),
+    "====================> Boost.PFR: Your compiler can not handle C++17 structured bindings. Read the above comments for workarounds."
+);
+#endif // #ifndef _MSC_VER
+
+template <class T>
+constexpr auto tie_as_tuple(T& val) noexcept {
   static_assert(
     !std::is_union<T>::value,
     "====================> Boost.PFR: For safety reasons it is forbidden to reflect unions. See `Reflection of unions` section in the docs for more info."
   );
-
-  GuaranteedSimpleAggregate test{true};
-  const auto& [a] = test; // ====================> Boost.PFR: Your compiler can not handle C++17 structured bindings. Read the below comments for workarounds.
-  (void)a;
-
-  /****************************************************************************
-  *
-  * It looks like your compiler or Standard Library can not handle C++17
-  * structured bindings.
-  *
-  * Workaround: Define BOOST_PFR_USE_CPP17 to 0
-  * It will disable the C++17 features for Boost.PFR library.
-  *
-  * Sorry for the inconvenience caused.
-  *
-  ****************************************************************************/
-
   typedef size_t_<boost::pfr::detail::fields_count<T>()> fields_count_tag;
   return boost::pfr::detail::tie_as_tuple(val, fields_count_tag{});
 }
