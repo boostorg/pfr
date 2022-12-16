6a38a072853a242055921e499b5dced21eb49fe4
denzor200 <denismikhaylov38@gmail.com>
2022-12-17 00:35:56 +0400

    Add test 'boost_no_pfr_macro'

---
+++ test/compile-fail/boost_no_pfr_macro.cpp
@@ -0,0 +1,10 @@
+// Copyright (c) 2022 Denis Mikhailov
+//
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
+#define BOOST_NO_PFR 1
+#include <boost/pfr/core.hpp>
+
+int main() { }
+
