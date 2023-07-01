9ac24dc00c02c31e62823abf5028867353d7a5f6
denzor200 <denismikhaylov38@gmail.com>
2023-07-01 14:28:26 +0000

    fix template_constructor.cpp for cxx20

--- test/core/run/template_constructor.cpp
+++ test/core/run/template_constructor.cpp
@@ -12,11 +12,7 @@ struct constrained_template {
     constrained_template() = default;
 
     template <
-      class U = T,
-      std::enable_if_t<
-          std::is_constructible<T, U&&>::value
-          || sizeof(decltype(T{std::declval<U&&>()}))
-      , bool> = false>
+      class U = T>
     constexpr constrained_template(U&& val)
         : value_{std::forward<U>(val)}
     {}
