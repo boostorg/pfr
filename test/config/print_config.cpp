b1432d5fa9cc4649ce7615653a256064fb735129
denzor200 <denismikhaylov38@gmail.com>
2023-09-23 01:52:03 +0300

    Fix for printing BOOST_PFR_FUNCTION_SIGNATURE

--- test/config/print_config.cpp
+++ test/config/print_config.cpp
@@ -3,7 +3,10 @@
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
-#include <boost/pfr/config.hpp> // inclusion of an another PFR header may fail when BOOST_PFR_ENABLED=0
+#include <boost/pfr/config.hpp>
+#if BOOST_PFR_ENABLED
+#include <boost/pfr.hpp>
+#endif
 #include <boost/preprocessor/stringize.hpp>
 
 #include <iostream>
