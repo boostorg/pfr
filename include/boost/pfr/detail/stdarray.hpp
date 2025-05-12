db9451143a70cbd30ce5a72f4aa294d73c17b7ab
Antony Polukhin <antoshkka@gmail.com>
2025-05-12 19:34:40 +0300

    Fix build on standards before C++20

--- include/boost/pfr/detail/stdarray.hpp
+++ include/boost/pfr/detail/stdarray.hpp
@@ -13,18 +13,27 @@
 
 #if !defined(BOOST_PFR_INTERFACE_UNIT)
 #include <array>
-#include <string_view>
+#if BOOST_PFR_CORE_NAME_ENABLED
+#   include <string_view>
+#endif
 #include <utility> // metaprogramming stuff
 #endif
 
 namespace boost { namespace pfr { namespace detail {
 
+#if BOOST_PFR_CORE_NAME_ENABLED
 template <class T, std::size_t... I>
 constexpr auto make_stdarray_from_tietuple(const T& t, std::index_sequence<I...>) noexcept {
     return std::array<std::string_view, sizeof...(I)>{
         boost::pfr::detail::sequence_tuple::get<I>(t)...
     };
 }
+#else
+template <class T, std::size_t... I>
+constexpr auto make_stdarray_from_tietuple(const T&, std::index_sequence<I...>) noexcept {
+    return nullptr;
+}
+#endif
 
 }}} // namespace boost::pfr::detail
 
