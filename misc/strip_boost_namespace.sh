228922fdb521224f0fcab969595cba6ae816d3ac
Antony Polukhin <antoshkka@gmail.com>
2026-01-04 11:26:11 +0300

    Update copyright years

--- misc/strip_boost_namespace.sh
+++ misc/strip_boost_namespace.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2021-2025 Antony Polukhin
+# Copyright (c) 2021-2026 Antony Polukhin
 #
 # Distributed under the Boost Software License, Version 1.0. (See accompanying
 # file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
--- test/core/compile-fail/inherited.cpp
+++ test/core/compile-fail/inherited.cpp
@@ -1,5 +1,5 @@
 // Copyright (c) 2021 Denis Mikhailov
-// Copyright (c) 2021 Antony Polukhin
+// Copyright (c) 2021-2026 Antony Polukhin
 //
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
