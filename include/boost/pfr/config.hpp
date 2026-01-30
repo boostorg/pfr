114ef6106040a1427730e1abb986b48ae56e672d
Antony Polukhin <antoshkka@gmail.com>
2026-01-30 12:03:53 +0300

    C++26 reflection based implementation

--- include/boost/pfr/config.hpp
+++ include/boost/pfr/config.hpp
@@ -61,6 +61,14 @@
 #endif
 #endif
 
+#ifndef BOOST_PFR_USE_CPP26_REFLECTION
+#ifdef __cpp_lib_reflection
+#define BOOST_PFR_USE_CPP26_REFLECTION 1
+#else
+#define BOOST_PFR_USE_CPP26_REFLECTION 0
+#endif
+#endif
+
 #ifndef BOOST_PFR_USE_CPP17
 #   ifdef __cpp_structured_bindings
 #       define BOOST_PFR_USE_CPP17 1
