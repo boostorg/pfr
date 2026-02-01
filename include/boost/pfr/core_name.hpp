31f7680a401e19bd7a16e9ffe669fd675043d2ad
Antony Polukhin <antoshkka@gmail.com>
2026-02-01 13:52:34 +0300

    fix

--- include/boost/pfr/core_name.hpp
+++ include/boost/pfr/core_name.hpp
@@ -108,14 +108,14 @@ template <class T, class F>
 constexpr void for_each_field_with_name(T&& value, F&& func) {
     return boost::pfr::detail::for_each_field(
         std::forward<T>(value),
-        [f = std::forward<F>(func)](auto&& field, auto index) mutable {
+        [&func](auto&& field, auto index) mutable {
             using IndexType = decltype(index);
             using FieldType = decltype(field);
             constexpr auto name = boost::pfr::detail::get_name<std::remove_reference_t<T>, IndexType::value>();
             if constexpr (std::is_invocable_v<F, std::string_view, FieldType, IndexType>) {
-                f(name, std::forward<FieldType>(field), index);
+                std::forward<F>(func)(name, std::forward<FieldType>(field), index);
             } else {
-                f(name, std::forward<FieldType>(field));
+                std::forward<F>(func)(name, std::forward<FieldType>(field));
             }
         });
 }
